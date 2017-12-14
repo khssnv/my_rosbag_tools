79641e48ff9bc8dcd06e162033fdb0ea103d645d
Alexander Krupenkin <mail@akru.me>
2017-12-14 11:38:21 +0300

    Added CORS, updated API

--- httpbag.py
+++ httpbag.py
@@ -5,11 +5,14 @@
 
 
 from datetime import date, timedelta
+from dateutil.parser import parse
 from flask import Flask
 from flask import request
+from flask_cors import CORS
 import rosbag, json, os
 
 app = Flask(__name__)
+CORS(app)
 
 def bag_to_str(path):
     bag = rosbag.Bag(path)
@@ -29,17 +32,18 @@ def str_to_dates(ds): # dates string
     until = date(int(ds[25:29]), int(ds[22:24]), int(ds[19:21]))
     return [since, until]
 
-@app.route('/')
-def main():
+@app.route('/api/v0/bag/<bag_from>/<bag_to>')
+def main(bag_from, bag_to):
     """
     Expected bag files like 2017-12-13-14-24-38.bag
     """
-    d1, d2 = str_to_dates(request.args.get('date'))
-    days = [ d1 + timedelta(days=x) for x in range((d2 - d1).days + 1) ]
+    d1 = parse(bag_from)
+    d2 = parse(bag_to)
+    days = [d1 + timedelta(days=x) for x in range((d2-d1).days + 1)]
     paths = [ '{0}-{1}-{2}-{3}-{4}-{5}.bag'.format(d.year, d.month, d.day, h, m, s)
-                for d in days for h in range(0, 24) for m in range(0, 60) for s in range(0, 60) ]
+                for d in days for h in range(0, 25) for m in range(0, 60) for s in range(0, 60) ]
     content = [ bag_to_str(p) for p in paths if os.path.isfile(p) ]
     return ''.join(content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=30000)
+    app.run()
