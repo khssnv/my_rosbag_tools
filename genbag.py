08e42cc6d752aa33fd76ca9548335383e75fcaa8
Alisher A. Khassanov <a.khssnv@gmail.com>
2018-12-04 19:27:53 +0400

    Shorter genbag

--- genbag.py
+++ genbag.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import rosbag
-from robonomics_game_transport.msg import TransportAction, TransportActionGoal, TransportGoal
+from std_msgs.msg import String
 
-colors = ['yellow', 'green', 'blue', 'purple']
-addresses = [1, 2, 3, 4]
-topic = '/transport_goods/storage/goal'
 
-for (addr, color) in [(addr, color) for addr in addresses for color in colors]:
-    bag = rosbag.Bag('./storage/addr-%d_color-%s.bag' % (addr, color), 'w')
-    try:
-        msg = TransportActionGoal(goal=TransportGoal(address=addr, specification=color))
-        bag.write(topic, msg)
-    finally:
-        bag.close()
+bag = rosbag.Bag('./mybag.bag', 'w')
+msg = String(data='mystring')
+
+try:
+    bag.write(topic, msg)
+finally:
+    bag.close()
